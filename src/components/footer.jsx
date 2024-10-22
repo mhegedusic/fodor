dc9ecae39e47af0a8d22ae5b19337b0d55e80394
mhegedusic <74006733+mhegedusic@users.noreply.github.com>
2024-10-22 14:11:12 +0200

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       
       <a href="https://www.facebook.com/people/Namje%C5%A1taj-Fodor/61560944850350/" target='_blank'><img src="https://i.ibb.co/hWkXQjf/face-logo.webp" alt="facelogo"/></a>
       <a href="https://www.facebook.com/people/Namje%C5%A1taj-Fodor/61560944850350/" target='_blank'><img src="https://i.ibb.co/1LyLYGK/instalogo.png" alt="instalogo" /></a>
-      <a href="mailto: ivanfodor777@gmail.com"><img src="https://i.ibb.co/6RHMdW7/maillogo.png" alt="maillogo" /></a>
+      <a href="mailto: ifodor777@gmail.com"><img src="https://i.ibb.co/6RHMdW7/maillogo.png" alt="maillogo" /></a>
       <p>&copy; 2024 Namještaj Fodor. Sva prava zadržana.</p>
       <hr />
       <strong>Vlasnik: Ivan Fodor <br />
