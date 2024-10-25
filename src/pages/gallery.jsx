d497739e429bcfc12335c07ebbfbc519a9cbcd4f
mhegedusic <mhegedusic@gmail.com>
2024-10-25 12:35:50 +0200

    add styles in gallery

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ImageGallery from 'react-image-gallery';
 import "react-image-gallery/styles/css/image-gallery.css";
+import styles from './Contact.module.css';
 
 const Gallery = ({ images }) => {
   // Formatiraj slike za react-image-gallery
