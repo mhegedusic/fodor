4487929097d8ce71b31ffb1949cf539b0aef32b7
mhegedusic <mhegedusic@gmail.com>
2024-10-25 08:19:37 +0200

    changing import styles in app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import  {BrowserRouter  as Router} from 'react-router-dom';
 import Header from './components/header';
 import Footer from './components/footer';
 import Main from './components/main';
-import styles from './Contact.module.css';
+import styles from './pages/Contact.module.css';
 
 
 const App = () => {
