9eab0d7ce0dc0381d295e14f42da451d486c9786
mhegedusic <mhegedusic@gmail.com>
2024-10-25 07:08:58 +0200

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Router from 'react-router-dom';
+import  {BrowserRouter  as Router} from 'react-router-dom';
 import Header from './components/header';
 import Footer from './components/footer';
 import Main from './components/main';
