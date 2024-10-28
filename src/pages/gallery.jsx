5e42345fff8e9cc7e8868b749b1c21f69b017d70
mhegedusic <mhegedusic@gmail.com>
2024-10-28 13:18:03 +0100

    remove desc. from galery

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -7,62 +7,50 @@ const images = [
   {
     original: 'https://i.ibb.co/YLLX0Bq/kuhinjaskrivenimehanizam.jpg',
     thumbnail: 'https://i.ibb.co/YLLX0Bq/kuhinjaskrivenimehanizam.jpg',
-    description: 'Opis slike 1',
   },
   {
     original: 'https://i.ibb.co/NnFpHYf/kuhinjazg.jpg',
     thumbnail: 'https://i.ibb.co/NnFpHYf/kuhinjazg.jpg',
-    description: 'Opis slike 2',
   },
   {
     original: 'https://i.ibb.co/f0YBxtT/kupaonica2.jpg',
     thumbnail: 'https://i.ibb.co/f0YBxtT/kupaonica2.jpg',
-    description: 'Opis slike 3',
   },
   {
     original: 'https://i.ibb.co/Y78dKBT/zidnostropnaoblogairasvjeta.jpg',
     thumbnail: 'https://i.ibb.co/Y78dKBT/zidnostropnaoblogairasvjeta.jpg',
-    description: 'Opis slike 3',
   },
   {
     original: 'https://i.ibb.co/svRYKwc/dekoracijaledrasvjeta.jpg',
     thumbnail: 'https://i.ibb.co/svRYKwc/dekoracijaledrasvjeta.jpg',
-    description: 'Opis slike 3',
   },
   {
     original: 'https://i.ibb.co/CbcvsJq/kuhinjaspogledom.jpg',
     thumbnail: 'https://i.ibb.co/CbcvsJq/kuhinjaspogledom.jpg',
-    description: 'Opis slike 3',
   },
   {
     original: 'https://i.ibb.co/sy6Fr8t/ormar1.jpg',
     thumbnail: 'https://i.ibb.co/sy6Fr8t/ormar1.jpg',
-    description: 'Opis slike 3',
   },
   {
     original: 'https://i.ibb.co/R6fX320/ormar2.jpg',
     thumbnail: 'https://i.ibb.co/R6fX320/ormar2.jpg',
-    description: 'Opis slike 3',
   },
   {
     original: 'https://i.ibb.co/1mC3BCG/komoda.jpg',
     thumbnail: 'https://i.ibb.co/1mC3BCG/komoda.jpg',
-    description: 'Opis slike 3',
   },
   {
     original: 'https://i.ibb.co/LNWgBv8/krevet.jpg',
     thumbnail: 'https://i.ibb.co/LNWgBv8/krevet.jpg',
-    description: 'Opis slike 3',
   },
   {
     original: 'https://i.ibb.co/BLy8hqT/ladice.jpg',
     thumbnail: 'https://i.ibb.co/BLy8hqT/ladice.jpg',
-    description: 'Opis slike 3',
   },
   {
     original: 'https://i.ibb.co/MV50YN5/kupaonica.jpg',
     thumbnail: 'https://i.ibb.co/MV50YN5/kupaonica.jpg',
-    description: 'Opis slike 3',
   },
   // Dodajte više slika po potrebi
 ];
