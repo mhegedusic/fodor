9cd5255f4d8d3439da99b03d5bba6ee56d1dcd45
mhegedusic <mhegedusic@gmail.com>
2024-10-28 07:13:22 +0100

    deleting gallery.jsx

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../pages/home';
 import About from '../pages/about';
-import Gallery from '../pages/gallery';
 import Contact from '../pages/contact';
 
 
@@ -12,7 +11,6 @@ const Main = ({ images }) => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/gallery" element={<Gallery images={images}/>} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </main>
